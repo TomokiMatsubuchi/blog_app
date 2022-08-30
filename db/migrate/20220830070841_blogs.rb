8f9aa9997e3cf0f228fff8657ad610164d625565
tomoki matsubuchi <matsubuchitmk@gmail.com>
2022-08-30 22:04:01 +0900

    再提出

--- db/migrate/20220830070841_blogs.rb
+++ db/migrate/20220830070841_blogs.rb
@@ -1,4 +1,8 @@
 class Blogs < ActiveRecord::Migration[6.1]
   def change
+    create_table :blogs do |t|
+      t.string :tilte
+      t.text :content
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,8 @@ ActiveRecord::Schema.define(version: 2022_08_30_070841) do
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
-    t.string "title"
+    t.string "tilte"
     t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
